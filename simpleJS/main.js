c07974d70365e2ceb6b47187d051405304ff4460
Adan Abbi <adanweli@hotmail.com>
2022-02-19 23:41:13 -0800

    functions

--- simpleJS/main.js
+++ simpleJS/main.js
@@ -1,1 +1,10 @@
-console.log("hello");
+function cutPieces(cutPone){
+  return cutPieces * 4;
+}
+
+function fruitMix(fruitOne, fruitTwo){
+  const firstFruitCuts = cutPieces(fruitOne)
+  const secondFruitCuts = cutPieces(fruitOne)
+
+  const juice = `you will need ${firs} ${}`
+}
